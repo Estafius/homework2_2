7b598b22f69f33555064dddd95c9d0be120d98cc
Estafius <estafius@gmail.com>
2017-03-10 22:02:40 +0200

    no message

--- json_read.py
+++ json_read.py
@@ -1,33 +1,67 @@
 import json
+import collections
+
+d = collections.defaultdict(int)
 
 with open('newsafr.json', encoding='utf-8') as newsafr:
  data_newsafr = json.load(newsafr)
  dictlist_newsafr = []
  for key, value in data_newsafr.items():
-    temp = [key,value]
-    dictlist_newsafr.append(temp)
+   for c in value['channel']['item']:
+    for i in c['description']['__cdata'].split(" "):
+      if len(i) > 6:
+       d[i] += 1
+      else:
+         continue
+
+newsafr = sorted(d.items(), key=lambda x: x[1], reverse=True)[0]
+
+d = collections.defaultdict(int)
 
 with open('newscy.json', encoding='utf-8') as newscy:
  data_newscy = json.load(newscy)
- dictlist_newscy = []
  for key, value in data_newscy.items():
-    temp = [key,value]
-    dictlist_newscy.append(temp)
-    
+   for c in value['channel']['item']:
+    for i in c['description']['__cdata'].split(" "):
+      if len(i) > 6:
+       d[i] += 1
+      else:
+         continue
+
+newscy = sorted(d.items(), key=lambda x: x[1], reverse=True)[0]
+
+d = collections.defaultdict(int)
+	
 with open('newsfr.json', encoding='utf-8') as newsfr:
  data_newsfr = json.load(newsfr)
- dictlist_newsfr = []
  for key, value in data_newsfr.items():
-    temp = [key,value]
-    dictlist_newsfr.append(temp)
+   for c in value['channel']['item']:
+    for i in c['description']['__cdata'].split(" "):
+      if len(i) > 6:
+       d[i] += 1
+      else:
+         continue
+
+newsfr = sorted(d.items(), key=lambda x: x[1], reverse=True)[0]
     
-    
+   
+d = collections.defaultdict(int)
+   
+
 with open('newsit.json', encoding='utf-8') as newsit:
  data_newsit = json.load(newsit)
- dictlist_newsit = []
  for key, value in data_newsit.items():
-    temp = [key,value]
-    dictlist_newsit.append(temp)
-    
-print(dictlist_newsafr,'\n\n\n',dictlist_newscy, '\n\n\n', dictlist_newsfr,'\n\n\n', dictlist_newsit)
+   for c in value['channel']['item']:
+    for i in c['description']['__cdata'].split(" "):
+      if len(i) > 6:
+       d[i] += 1
+      else:
+         continue
 
+newsit = sorted(d.items(), key=lambda x: x[1], reverse=True)[0]   
+   
+
+print('Самое частое слово для Новостей из файла newsafr.json: ', newsafr)
+print('\nСамое частое слово для Новостей из файла newscy.json: ', newscy)
+print('\nСамое частое слово для Новостей из файла newsfr.json: ', newsfr)
+print('\nСамое частое слово для Новостей из файла newsit.json: ', newsit)
