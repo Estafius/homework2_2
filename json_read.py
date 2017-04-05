16e659212e40f8a6abe98213aec61683bdc5c598
Estafius <estafius@gmail.com>
2017-04-05 20:51:04 +0300

    Add files via upload

--- json_read.py
+++ json_read.py
@@ -2,35 +2,30 @@ import json
 import collections
 import chardet
 
-def get_popular_words(file,encoding):
+
+def get_popular_words(file, encoding):
  d = collections.defaultdict(int)
- with open(file, encoding= encoding) as json_content:
+ with open(file, encoding=encoding) as json_content:
   data_file = json.load(json_content)
-  if file != 'newsit.json':
-   for key, value in data_file.items():
-    for c in value['channel']['item']:
-     for i in c['description']['__cdata'].split(" "):
+  for i in data_file['rss']['channel']['item']:
+     if isinstance(i['description'], str):
+      description = i['description']
+     else:
+      description = i['description']['__cdata']
+     for i in description.split(" "):
        if len(i) > 6:
-        d[i] += 1
+         d[i] += 1
        else:
         continue
-  else:
-    for value in data_file['rss']['channel']['item']:
-     for i in value['description'].split(" "):
-      if len(i) > 6:
-        d[i] += 1
-      else:
-         continue
- top_10 = sorted(d.items(), key=lambda x: x[1], reverse=True)[0:10]
- return top_10
-
+  top_10 = sorted(d.items(), key=lambda x: x[1], reverse=True)[0:10]
+  return top_10
 
 
 def main():
- list_file = ['newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json']
+ list_file = ['newsafr.json', 'newscy.json', 'newsit.json', 'newsfr.json']
  for file in list_file:
   rawdata = open(file, 'rb').read()
   result = chardet.detect(rawdata)
-  top_10= get_popular_words(file,result['encoding'])
-  print('\n10 самых часто употребляемых слов из файла ',file,': ', top_10,'\n')
+  top_10 = get_popular_words(file, result['encoding'])
+  print('\n10 самых часто употребляемых слов из файла ', file, ': ', top_10, '\n')
 main()
